191961b10d3962a3298230b217d3ba650b8c55db
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-09-08 00:05:05 -0700

    Updating the vapor version in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Resume",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.1"),
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
